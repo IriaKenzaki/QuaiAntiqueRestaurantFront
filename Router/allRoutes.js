14abd0f2fe210108527cf8d4fb3646b256c2234d
IriaKenzaki <myriam-iria@hotmail.fr>
2024-08-28 11:31:30 +0200

    valider les critères d'inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html","/js/auth/signup.js"),
     new Route("/account","Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword","Modifer mon mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allResa","Vos réservation", "/pages/reservation/allResa.html"),
