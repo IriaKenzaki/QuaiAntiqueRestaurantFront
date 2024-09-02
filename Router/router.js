cc6f3acc942059bd9edca32299190375b6415989
IriaKenzaki <myriam-iria@hotmail.fr>
2024-09-02 15:31:29 +0200

    maj

--- Router/router.js
+++ Router/router.js
@@ -1,7 +1,7 @@
-import Route from "./route.js";
+import route from "./route.js";
 import { allRoutes, websiteName } from "./allRoutes.js";
 // Création d'une route pour la page 404 (page introuvable)
-const route404 = new Route("404", "Page introuvable", "/pages/404.html", []);
+const route404 = new route("404", "Page introuvable", "/pages/404.html", []);
 
 // Fonction pour récupérer la route correspondant à une URL donnée
 const getRouteByUrl = (url) => {
