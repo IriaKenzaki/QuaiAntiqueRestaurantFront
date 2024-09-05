11ece25e09bf120de1d9113c3f82d373a27dcde3
IriaKenzaki <myriam-iria@hotmail.fr>
2024-09-05 22:34:14 +0200

    maj allroute

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,8 @@ export const allRoutes = [
     new Route("/galerie", "La galerie", "/pages/galerie.html", [], "/js/galerie.js"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html", ["ROLE_USER", "admin"]),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["ROLE_USER", "admin"]),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", ["ROLE_USER", "ROLE_ADMIN"]),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["ROLE_USER", "ROLE_ADMIN"]),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["ROLE_USER"]),
     new Route("/reserver", "Réserver", "/pages/reservations/reserver.html", ["ROLE_USER"]),
 ];
